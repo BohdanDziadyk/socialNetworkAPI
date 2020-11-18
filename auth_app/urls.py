c6266e865c5b800a44a22567e332c2307afd115e
BohdanDziadyk <bohdan.reaper669@gmail.com>
2020-11-18 13:19:07 +0200

    checkpoint commit, refresh view updated

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-from auth_app.views import RegisterView
+from auth_app.views import RegisterView, TokenRefreshViewSpecial
 
 urlpatterns = [
     path('', TokenObtainPairView.as_view()),
-    path('refresh', TokenRefreshView.as_view()),
+    path('refresh', TokenRefreshViewSpecial.as_view()),
     path('register', RegisterView.as_view())
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,6 +1,9 @@
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
-
+from rest_framework.response import Response
+from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
+from rest_framework_simplejwt.views import TokenRefreshView
 from user.models import UserModel
 from user.serializers import UserSerializer
 
@@ -11,3 +14,19 @@ class RegisterView(CreateAPIView):
     permission_classes = [AllowAny]
     serializer_class = UserSerializer
     queryset = UserModel.objects.all()
+
+
+class InvalidTokenSpecial(InvalidToken):
+    status_code = status.HTTP_403_FORBIDDEN
+
+
+class TokenRefreshViewSpecial(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidTokenSpecial(e.args[0])
+
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
