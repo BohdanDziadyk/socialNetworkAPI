9f496768e6d67c174c7893e0ca4ed171ccaa48c6
BohdanDziadyk <bohdan.dziadyk1@gmail.com>
2021-01-04 17:35:18 +0200

    added image field to comment model, photos upload fixed

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db import models
 
 from post.models import PostModel
@@ -5,6 +7,9 @@ from user.models import UserModel
 
 
 # Create your models here.
+def images_directory_path(instance, filename):
+    return os.path.join(f'{instance.user.username}', 'comments', filename)
+
 
 class CommentModel(models.Model):
     class Meta:
@@ -13,5 +18,6 @@ class CommentModel(models.Model):
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE, related_name='comment')
     post = models.ForeignKey(PostModel, on_delete=models.CASCADE, related_name='comment')
     body = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to=images_directory_path)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
